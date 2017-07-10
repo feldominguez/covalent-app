20ddabe8fb17957ce0fac2834b630beefc55b5e2
fel <fel@felipes-MacBook-Pro.local>
2017-07-10 03:36:05 -0700

    covalen app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
+import { CovalentLayoutModule } from '@covalent/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  routes: Object[] = [{
+      icon: 'home',
+      route: '.',
+      title: 'Home',
+    }, {
+      icon: 'library_books',
+      route: '.',
+      title: 'Documentation',
+    }, {
+      icon: 'color_lens',
+      route: '.',
+      title: 'Style Guide',
+    }, {
+      icon: 'view_quilt',
+      route: '.',
+      title: 'Layouts',
+    }, {
+      icon: 'picture_in_picture',
+      route: '.',
+      title: 'Components & Addons',
+    },
+  ];
+  usermenu: Object[] = [{
+      icon: 'swap_horiz',
+      route: '.',
+      title: 'Switch account',
+    }, {
+      icon: 'tune',
+      route: '.',
+      title: 'Account settings',
+    }, {
+      icon: 'exit_to_app',
+      route: '.',
+      title: 'Sign out',
+    },
+  ];
+
+    constructor(private _iconRegistry: MdIconRegistry,
+              private _domSanitizer: DomSanitizer) {
+    this._iconRegistry.addSvgIconInNamespace('assets', 'teradata',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata.svg'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+
+import { MdCardModule, MdIconModule, MdButtonModule, MdIconRegistry, MdIcon, MdListModule } from '@angular/material';
+
+
+import { CovalentLayoutModule, CovalentStepsModule, CovalentCommonModule } from '@covalent/core';
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +22,22 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    MdCardModule,
+    CommonModule,
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule,
+    MdButtonModule,
+    MdIconModule,
+    MdListModule,
+    CovalentCommonModule
   ],
-  providers: [],
+  providers: [MdIconRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
